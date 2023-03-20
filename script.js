03486aa8eb56629182e79ff3137a6d06a9750272
guru28272 <117896953+guru28272@users.noreply.github.com>
2023-03-20 19:00:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -10,6 +10,38 @@ const ageBtn = document.querySelector(`#age`);
 const emailBtn = document.querySelector(`#email`);
 const phnBtn = document.querySelector(`#phone`);
 
+fetch(url)
+  .then((data) => {
+    return data.json();
+  })
+  .then((res) => {
+    // console.log(res);
+    imag.src = res.results[0].picture.large;
+    let name = `${res.results[0].name.title}: ${res.results[0].name.first} ${res.results[0].name.last}`;
+    show.textContent = name;
+
+    let age = res.results[0].dob.age;
+    let email = res.results[0].email;
+    let phone = res.results[0].cell;
+
+    data.dataset.age = age;
+    data.dataset.email = email;
+    data.dataset.phone = phone;
+
+    ageBtn.addEventListener(`click`, () => {
+      display.innerHTML = ``;
+      display.innerHTML = age;
+    });
+    emailBtn.addEventListener(`click`, () => {
+      display.innerHTML = ``;
+      display.innerHTML = email;
+    });
+    phnBtn.addEventListener(`click`, () => {
+      display.innerHTML = ``;
+      display.innerHTML = phone;
+    });
+  });
+
 async function getData(params) {
   display.innerHTML = ``;
 
