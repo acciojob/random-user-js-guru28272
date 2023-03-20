cf21d5bdbbdbee11acc13e965653d1faaca1d509
guru28272 <117896953+guru28272@users.noreply.github.com>
2023-03-20 18:31:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,55 @@
 //your code here
+let url = `https://randomuser.me/api/`;
+const data = document.querySelector(`#data`);
+const show = document.querySelector(`#show`);
+const display = document.querySelector(`#display`);
+
+const imag = document.querySelector(`#imag`);
+const getBtn = document.querySelector(`#getUser`);
+const ageBtn = document.querySelector(`#age`);
+const emailBtn = document.querySelector(`#email`);
+const phnBtn = document.querySelector(`#phone`);
+
+async function getData(params) {
+  display.innerHTML = ``;
+
+  let res = await fetchData(url);
+  // console.log(res);
+
+  imag.src = res.results[0].picture.large;
+  let name = `${res.results[0].name.title}: ${res.results[0].name.first} ${res.results[0].name.last}`;
+  show.textContent = name;
+
+  let age = res.results[0].dob.age;
+  let email = res.results[0].email;
+  let phone = res.results[0].cell;
+
+  data.dataset.age = age;
+  data.dataset.email = email;
+  data.dataset.phone = phone;
+
+  // console.log((data.dataset.age = age));
+  // console.log(data.dataset);
+
+  // console.log(`getData `, age);
+  ageBtn.addEventListener(`click`, () => {
+    display.innerHTML = ``;
+    display.innerHTML = age;
+  });
+  emailBtn.addEventListener(`click`, () => {
+    display.innerHTML = ``;
+    display.innerHTML = email;
+  });
+  phnBtn.addEventListener(`click`, () => {
+    display.innerHTML = ``;
+    display.innerHTML = phone;
+  });
+}
+
+async function fetchData(url) {
+  let res = await fetch(url);
+  let data = await res.json();
+
+  return data;
+}
+getBtn.addEventListener(`click`, getData);
